d84a0b566533b238c50919ab73a105282f2ea9db
rashikreza9-cyber <rashikreza9@gmail.com>
2026-04-22 22:59:38 +0600

    Update game.py

--- pset 04/game.py
+++ pset 04/game.py
@@ -1,8 +1,15 @@
-import random
 while True:
     try:
-        level = int(input("Level: "))
-        if level > 0:
+        guess = int(input("Guess: "))
+        if guess < 1:
+            continue
+        
+        if guess < secret_number:
+            print("Too small!")
+        elif guess > secret_number:
+            print("Too large!")
+        else:
+            print("Just right!")
             break
     except ValueError:
         pass
